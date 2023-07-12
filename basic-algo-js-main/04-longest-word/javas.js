ae6bb06180537b279e381f0ae174bd4a472f9807
Possidius Capo <possidiusc@gmail.com>
2023-07-12 02:02:33 +0200

    exercice

--- basic-algo-js-main/04-longest-word/javas.js
+++ basic-algo-js-main/04-longest-word/javas.js
@@ -1,19 +1,27 @@
 `use strict`
 
 function findLongestWordLength(str) {
-  let convertStrInTable = str.split('');
-  console.log(convertStrInTable)
-  // for (let i = 0; i < convertStrInTable.length; i++) {
-  //   let longueur = convertStrInTable[i].length;
-  //   console.log(Math.max(longueur)); 
-  // }
+  // convertir la phrase en un tableau donc par mots
+  let convertStrInTable = str.split(' ');
+  // créer un tableau vide di
+  let tab = [];
+  // console.log(convertStrInTable)
+  for (let i = 0; i < convertStrInTable.length; i++) {
+    let longueur = convertStrInTable[i].length;
+    
+    tab = tab.push(convertStrInTable[i].length)
+    console.log(tab[i]);
+    // console.log(longueur);
+    console.log(Math.max(maxStr)); 
+  }
   // return str[i].length;
 }
   // findLongestWordLength();
   findLongestWordLength("The quick brown fox jumped over the lazy dog");
 
-let str = 'the quick brown fox jumped over the lazy dog'
-
+// let str = 'the quick brown fox jumped over the lazy dog'
+// let tab = str.split(' ')
+// console.log(tab);
 
 // let longueur = convertStrInTable[i].length();
-  console.log(convertStrInTable);
+  // console.log(convertStrInTable);
