4b6341ea6db575f3afca107efb6678acde44af1c
Possidius Capo <possidiusc@gmail.com>
2023-07-14 18:59:55 +0100

    nouvelle notion sur les options vuejs

---
+++ FramWork/vue-playground/10-site/js/footer.js
@@ -0,0 +1,15 @@
+`user strict`
+
+export default {
+    template:/*html */`
+    <footer >
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, quaerat?</p>
+        <ul>
+            <li><a href="#">Lorem.</a></li>
+            <li><a href="#">Repudiandae.</a></li>
+            <li><a href="#">Atque.</a></li>
+            <li><a href="#">Alias?</a></li>
+            <li><a href="#">Nobis.</a></li>
+        </ul>
+    </footer>`
+}
---
+++ FramWork/vue-playground/10-site/js/header.js
@@ -0,0 +1,17 @@
+`user strict`
+
+export default {
+    template:/*html */`
+    <header class="header">
+    <h1>LOGO</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Lorem.</a></li>
+            <li><a href="#">Repudiandae.</a></li>
+            <li><a href="#">Atque.</a></li>
+            <li><a href="#">Alias?</a></li>
+            <li><a href="#">Nobis.</a></li>
+        </ul>
+    </nav>
+</header>`
+}
